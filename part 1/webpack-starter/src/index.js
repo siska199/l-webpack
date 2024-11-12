cd448c4d6ffd5aa2cbe6e90d3f9794bfc3233fc9
siska199 <siska.apriana.1703226@students.um.ac.id>
2024-11-12 17:10:03 +0700

    learn: webpack part-1.1

---
+++ part 1/webpack-starter/src/index.js
@@ -0,0 +1,10 @@
+import generateJoke from "./generate-joke";
+import './styles/main.scss'
+import bg from './assets/bg.svg'
+
+
+const bgImg = document.getElementById('bgImg')
+
+bgImg.src = bgImg
+console.log(generateJoke())
+console.log(1)
